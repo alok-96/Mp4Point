5b01398371e3f8cbfff1489ad0e64b2683a6d5cc
alok96 <dwivedialok799@gmail.com>
2023-07-19 08:38:30 +0530

    updated social links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ import {
   AiFillInstagram,
   AiFillGithub,
   AiFillTwitterCircle,
+  AiFillLinkedin
 } from "react-icons/ai";
 import { HStack, Stack, VStack, Image, Text, Heading } from "@chakra-ui/react";
 
@@ -33,11 +34,41 @@ const Footer = () => {
         justifyContent={["center", "center", "flex-end"]}
         gap={"30px"}
       >
-        <AiFillCodepenCircle size={"30"} />
-        <AiFillFacebook size={"30"} />
-        <AiFillTwitterCircle size={"30"} />
-        <AiFillInstagram size={"30"} />
-        <AiFillGithub size={"30"} />
+        <a
+          href="https://www.linkedin.com/in/alok-dwivedi-4347801b8/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillLinkedin size={"30"} />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100070380284005"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillFacebook size={"30"} />
+        </a>
+        <a
+          href="https://twitter.com/alokdwivedi99"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillTwitterCircle size={"30"} />
+        </a>
+        <a
+          href="https://www.instagram.com/_its_alok_07_/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillInstagram size={"30"} />
+        </a>
+        <a
+          href="https://github.com/alok-96"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillGithub size={"30"} />
+        </a>
       </HStack>
     </Stack>
   );
